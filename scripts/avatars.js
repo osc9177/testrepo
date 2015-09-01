7e6dee21efbdca22feed600d1d1fe06034d5654c
Young-won <youngwon08@gmail.com>
2015-09-01 14:58:36 +0900

    test repo

---
+++ scripts/avatars.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('.cs-student').each(function() {
+    var $student = $(this);
+    var username = $student.data('username');
+    var $avatar = $student.find('.cs-avatar');
+
+    $.ajax({
+      url: 'https://api.github.com/users/' + username,
+      dataType: 'json',
+      timeout: 2000,
+      cache: true,
+      success: function(data) {
+        $avatar.attr('src', data.avatar_url);
+        var $name = $student.find('.cs-name');
+        $name.text(data.name);
+      },
+      error: function() {
+        $avatar.attr('src', 'https://github.com/identicons/' + username + '.png');
+      }
+    })
+  });
+});
